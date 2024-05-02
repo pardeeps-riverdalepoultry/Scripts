5b11253e292fb1e56b19dc619d2de0d9b9a47ffb
pardeeps <pardeeps@riverdalepoultry.ca>
2024-05-02 07:05:00 -0400

    Change2

--- CombineCSV.py
+++ CombineCSV.py
@@ -2,10 +2,10 @@ import os
 import pandas as pd
 
 # Folder path containing CSV files
-folder_path = r"G:\29.0 Data Infrastructure & Analytics\1.0 Projects\2024.03 DLB historical data migration\!Uploaded Months\february_2024_csv\Drivers"
+folder_path = r"C:\Users\Pardeep Singh\OneDrive - Riverdale Poultry Express\Documents\CSV Files"
 
 # Output CSV file path
-output_csv_path = "combined_data.csv"
+output_csv_path = r"C:\Users\Pardeep Singh\OneDrive - Riverdale Poultry Express\Documents\combined_data.csv"
 
 # Initialize an empty DataFrame to store combined data
 combined_df = pd.DataFrame()
@@ -15,7 +15,7 @@ for filename in os.listdir(folder_path):
     if filename.endswith(".csv"):
         file_path = os.path.join(folder_path, filename)
         df = pd.read_csv(file_path)
-        combined_df = combined_df.append(df, ignore_index=True)
+        combined_df = pd.concat([combined_df, df], ignore_index=True)
 
 # Write the combined data to a single CSV file
 combined_df.to_csv(output_csv_path, index=False)
